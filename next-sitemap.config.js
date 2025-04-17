012f5bf653a0ffbcd77ca515a9995182f5e8961d
kir4che <mollydcxxiii@gmail.com>
2025-04-17 12:28:35 +0800

    🐛 correct siteUrl in next-sitemap config using env variable

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,22 +1,30 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://kir4che.com',
+  siteUrl: process.env.NEXT_PUBLIC_API_URL || 'https://kir4che.com',
   generateRobotsTxt: true,
-  generateIndexSitemap: true,
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 5000,
-  exclude: [],
 
   // hreflang 設定
   alternateRefs: [
     {
-      href: 'https://kir4che.com/tw',
-      hreflang: 'zh-TW',
+      href: `${process.env.NEXT_PUBLIC_API_URL || 'https://kir4che.com'}/tw`,
+      hreflang: 'zh',
     },
     {
-      href: 'https://kir4che.com/en',
+      href: `${process.env.NEXT_PUBLIC_API_URL || 'https://kir4che.com'}/en`,
       hreflang: 'en',
     },
   ],
+
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
