23a646bb9ffa6c3e5e89f2f5e267dbd4280d64d4
kir4che <mollydcxxiii@gmail.com>
2025-04-18 17:04:32 +0800

    ✨ refactor PostPage to fetch post data from API and add rehypeExpressiveCode configuration

--- src/app/(blog)/[lang]/posts/[slug]/page.tsx
+++ src/app/(blog)/[lang]/posts/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import type { Metadata } from 'next';
-import { redirect } from 'next/navigation';
+import { redirect, notFound } from 'next/navigation';
 import { serialize } from 'next-mdx-remote/serialize';
 
 import rehypeSlug from 'rehype-slug';
@@ -13,68 +12,36 @@ import rehypeUnwrapImages from 'rehype-unwrap-images';
 import { remarkMark } from 'remark-mark-highlight';
 import remarkImages from 'remark-images';
 import rehypeExpressiveCode from 'rehype-expressive-code';
-import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
-import { createInlineSvgUrl } from '@expressive-code/core';
 
 import type { Language } from '@/types/language';
-import { getPostData } from '@/lib/posts';
+import { rehypeExpressiveCodeOptions } from '@/config/expressiveCode';
 
 import PostLayout from '@/components/features/post/PostLayout';
 import MDXContent from '@/components/mdx/MDXContent';
 
-type Props = {
+interface Props {
   params: Promise<{
     lang: Language;
     slug: string;
   }>;
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const lang = (await params).lang;
-  const slug = (await params).slug;
-
-  const post = await getPostData(lang, slug);
-
-  return {
-    title: post.title,
-    description: post.description,
-  };
 }
-
-const terminalIconMacStyle = createInlineSvgUrl(`
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="7" viewBox="0 0 34 10" preserveAspectRatio="xMidYMid meet">
-  <circle cx="5" cy="5" r="4" fill="#ff5f56"/>
-  <circle cx="17" cy="5" r="4" fill="#ffbd2e"/>
-  <circle cx="29" cy="5" r="4" fill="#27c93f"/>
-</svg>
-`);
-
-/** @type {import('rehype-expressive-code').RehypeExpressiveCodeOptions} */
-const rehypeExpressiveCodeOptions = {
-  themes: 'one-dark-pro',
-  frames: {
-    showCopyToClipboardButton: true,
-  },
-  styleOverrides: {
-    frames: {
-      terminalTitlebarBackground: '#2d2d2d',
-      terminalTitlebarBorderBottomColor: '#2d2d2d',
-      terminalIcon: terminalIconMacStyle,
-    },
-  },
-  defaultProps: {
-    // frame: 'terminal',
-    wrap: true,
-    showLineNumbers: false,
-  },
-  plugins: [pluginLineNumbers()],
-};
 
 const PostPage = async ({ params }: Props) => {
   const { lang, slug } = await params;
 
   try {
-    const post = await getPostData(lang, slug);
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/posts/${slug}`,
+      {
+        headers: {
+          'Accept-Language': lang,
+        },
+      }
+    );
+
+    if (!res.ok) return notFound();
+
+    const post = await res.json();
 
     // serialize 會將 MDX 內容轉換成 React 組件
     const mdxSource = await serialize(post.content, {
