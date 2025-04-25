45f826a743d7cce647aa35cdec63a307759bd146
kir4che <mollydcxxiii@gmail.com>
2025-04-26 00:50:22 +0800

    🗑️ clean up site content and remove unused pages

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,7 +3,6 @@ export const EMAIL = 'mollydcxxiii@gmail.com';
 
 export const SOCIAL_LINKS = {
   youtube: 'https://www.youtube.com/@kir4che',
-  twitch: 'https://www.twitch.tv/kir4che',
   github: 'https://github.com/kir4che',
   instagram: 'https://www.instagram.com/kir4che',
 } as const;
