e1b9a0cb07fa1f36b992df56d39b17a63655ba7c
kir4che <mollydcxxiii@gmail.com>
2025-04-30 04:01:56 +0800

    ♻️ refactor MDXContent component to simplify structure and improve readability

--- src/components/mdx/MDXContent.tsx
+++ src/components/mdx/MDXContent.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { memo } from 'react';
 import dynamic from 'next/dynamic';
 import { MDXRemote, type MDXRemoteSerializeResult } from 'next-mdx-remote';
 import { useMDXComponents } from '@/hooks/useMDXComponents';
@@ -10,17 +9,9 @@ interface MDXProps {
   imageMetas: Record<string, any>;
 }
 
-const MDXWrapper = memo(({ content, imageMetas }: MDXProps) => {
+function InnerMDXContent({ content, imageMetas }: MDXProps) {
   const components = useMDXComponents(imageMetas);
   return <MDXRemote {...content} components={components} />;
-});
+}
 
-MDXWrapper.displayName = 'MDXWrapper';
-
-const MDXContent = memo(({ content, imageMetas }: MDXProps) => {
-  return <MDXWrapper content={content} imageMetas={imageMetas} />;
-});
-
-MDXContent.displayName = 'MDXContent';
-
-export default dynamic(() => Promise.resolve(MDXContent), { ssr: false });
+export default dynamic(() => Promise.resolve(InnerMDXContent), { ssr: false });
