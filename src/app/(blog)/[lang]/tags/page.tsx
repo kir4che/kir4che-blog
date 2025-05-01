209bbf7d8335aef92a23fb6fcba5eebade9ca4cf
kir4che <mollydcxxiii@gmail.com>
2025-05-01 15:57:23 +0800

    ✨ add transition effects to multi components

--- src/app/(blog)/[lang]/tags/page.tsx
+++ src/app/(blog)/[lang]/tags/page.tsx
@@ -35,30 +35,35 @@ const TagsPage = async ({ params }: { params: Params }) => {
     return notFound();
   }
 
-  return (
-    <>
-      <h1 className='mb-4'>{t('title')}</h1>
-      <div className='flex flex-wrap items-center gap-x-4 gap-y-5'>
-        {tags.map(({ name, slug, postCount }) => (
-          <Link
-            key={slug}
-            href={`/tags/${slug}`}
-            className='bg-bg-secondary dark:bg-text-gray-dark transition-color flex w-40 items-center justify-between rounded-full p-3 shadow-[2px_2px_3px_rgba(0,0,0,0.05)] transition-all duration-150 hover:scale-105'
-          >
-            <h2 className='text-sm text-nowrap'># {name}</h2>
-            <span className='flex h-4 w-4 items-center justify-center rounded-full border border-pink-500 text-[10px] font-semibold text-pink-600 dark:border-pink-400 dark:text-pink-500'>
-              {postCount}
-            </span>
-          </Link>
-        ))}
-      </div>
-      {tags.length === 0 && (
-        <p className='text-text-gray dark:text-text-gray-lighter text-sm'>
-          {t('noTags')}
-        </p>
-      )}
-    </>
-  );
+    return (
+      <>
+        <h1 className='mb-4'>{t('title')}</h1>
+        {tags?.length ? (
+          <div className='flex flex-wrap items-center gap-x-4 gap-y-5 transition-colors duration-300'>
+            {tags.map(({ name, slug, postCount }) => (
+              <Link
+                key={slug}
+                href={`/tags/${slug}`}
+                className='bg-bg-secondary dark:bg-text-gray-dark flex w-40 items-center justify-between rounded-full p-3 shadow-[2px_2px_3px_rgba(0,0,0,0.05)] hover:scale-105'
+                aria-label={`${name} (${postCount} posts)`}
+              >
+                <h2 className='text-sm text-nowrap'># {name}</h2>
+                <span className='flex h-4 w-4 items-center justify-center rounded-full border border-pink-500 text-[10px] font-semibold text-pink-600 dark:border-pink-400 dark:text-pink-500'>
+                  {postCount}
+                </span>
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <p className='text-text-gray dark:text-text-gray-lighter text-sm'>
+            {t('noTags')}
+          </p>
+        )}
+      </>
+    );
+  } catch {
+    return notFound();
+  }
 };
 
 export default TagsPage;
