565d06d4e470c2f4bfa6305bdcd90b7ffdfc2ed1
kir4che <mollydcxxiii@gmail.com>
2025-07-16 23:10:48 +0800

    ✨ update youTube video list

--- src/config/youtubes.ts
+++ src/config/youtubes.ts
@@ -1,18 +1,18 @@
 const youtubes = {
   1: {
+    title: '五月份小聚 Waku Waku Burger (っᐛ)っ🍔 | Molly',
+    url: 'https://www.youtube.com/embed/M6r9dyYaXvI?si=HMIs4G5rXUWT8YOc',
+    date: '2025.06.26',
+  },
+  2: {
     title: '四月份小聚兩餐 & 友過生日 ˚˖𓍢 🥘˚⊹♡ | Molly',
     url: 'https://www.youtube.com/embed/JaHmaHsArjk?si=BRSBsDQ_mORvrq3G',
     date: '2025.04.20',
   },
-  2: {
+  3: {
     title: '殼以！一個人去 CFO 粉絲感謝會 ˖°⋆🦪₊ | Molly',
     url: 'https://www.youtube.com/embed/Q0gMMnbfCqU?si=TWyLv1SNABGmYFig',
     date: '2025.03.29',
-  },
-  3: {
-    title: '三月份小聚海底撈 🍲 ⊹₊⟡⋆ | Molly',
-    url: 'https://www.youtube.com/embed/yXoZyBJwIV4?si=MT0PrPFsmX54bC3q',
-    date: '2025.03.24',
   },
 };
 
