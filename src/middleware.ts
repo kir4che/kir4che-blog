4c7ce2e270ab0d9a29a2ce89eb580db7f923e1e2
kir4che <mollydcxxiii@gmail.com>
2025-08-18 15:12:28 +0800

    ✨ refactor middleware for improved locale detection and redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,29 @@
-// middleware.ts
 import { NextRequest, NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 
-import { CONFIG } from '@/config';
 import routing from '@/i18n/routing';
+import { CONFIG } from '@/config';
+import { detectLocale } from '@/utils/detectLocale';
 
+const LOCALES = CONFIG.languages.supportedLanguages;
 const DEFAULT_PATH = CONFIG.paths.languagePaths.tw;
 
 export const middleware = (request: NextRequest) => {
   const { pathname } = request.nextUrl;
 
+  // 若是根目錄，自動導向預設語言路徑。
   if (pathname === '/')
     return NextResponse.redirect(new URL(DEFAULT_PATH, request.url));
+
+  // 檢查目前 URL 是否缺少語言前綴
+  const isMissingLocale = LOCALES.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  );
+
+  if (isMissingLocale) {
+    const locale = detectLocale(request);
+    return NextResponse.redirect(new URL(`/${locale}${pathname}`, request.url));
+  }
 
   const isEditorPage = /^\/(tw|en)\/posts\/editor(?:\/|$)?/.test(pathname);
   const locale = pathname.split('/')[1];
@@ -21,9 +33,11 @@ export const middleware = (request: NextRequest) => {
   return createMiddleware(routing)(request);
 };
 
+// 作用的路徑範圍
 export const config = {
   matcher: [
-    '/(tw|en)/:path*',
+    '/(tw|en)/:path*', // 包含語系的路徑
+    // 其他需要導向的（不包含 api、靜態資源等）
     '/((?!api|_next|.*\\..*|favicon.ico|robots.txt|sitemap.xml).*)',
   ],
 };
