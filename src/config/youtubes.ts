18a7fc5bb1a63629e53796830e4f98923c4a8a71
kir4che <mollydcxxiii@gmail.com>
2025-09-15 02:18:12 +0800

    ✨ update youtube videos list

--- src/config/youtubes.ts
+++ src/config/youtubes.ts
@@ -1,18 +1,18 @@
 const youtubes = {
   1: {
+    title: '七月份小聚帕斯塔焗麵所 & 最愛拍貼店 GOOD KIDS CLUB 🧸ྀི | Molly',
+    url: 'https://www.youtube.com/embed/t_qMUo__cWE?si=hf7EiK1BooHcEWgP',
+    date: '2025.08.25',
+  },
+  2: {
+    title: '生日月小聚 Waku Waku Pasta 🍝| Molly',
+    url: 'https://www.youtube.com/embed/Xs1CVvvXvVM?si=wLrdOw2rmYhFHPYL',
+    date: '2025.08.20',
+  },
+  3: {
     title: '五月份小聚 Waku Waku Burger (っᐛ)っ🍔 | Molly',
     url: 'https://www.youtube.com/embed/M6r9dyYaXvI?si=HMIs4G5rXUWT8YOc',
     date: '2025.06.26',
-  },
-  2: {
-    title: '四月份小聚兩餐 & 友過生日 ˚˖𓍢 🥘˚⊹♡ | Molly',
-    url: 'https://www.youtube.com/embed/JaHmaHsArjk?si=BRSBsDQ_mORvrq3G',
-    date: '2025.04.20',
-  },
-  3: {
-    title: '殼以！一個人去 CFO 粉絲感謝會 ˖°⋆🦪₊ | Molly',
-    url: 'https://www.youtube.com/embed/Q0gMMnbfCqU?si=TWyLv1SNABGmYFig',
-    date: '2025.03.29',
   },
 };
 
