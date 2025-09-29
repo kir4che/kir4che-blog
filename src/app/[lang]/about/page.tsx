f83cb6b7978ccff78f22795d08693a6eb7b61238
kir4che <mollydcxxiii@gmail.com>
2025-09-29 19:30:05 +0800

    ♻️ refactor AboutPage to use client-side translations and improve rendering

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-static';
+'use client';
 
-import { getTranslations } from 'next-intl/server';
+import { useTranslations } from 'next-intl';
 import { motion } from 'motion/react';
 
 import { CONFIG } from '@/config';
@@ -28,8 +28,8 @@ const ANIMATIONS = {
   },
 };
 
-const AboutPage = async () => {
-  const t = await getTranslations('AboutPage');
+const AboutPage = () => {
+  const t = useTranslations('AboutPage');
 
   return (
     <div className='bg-bg-secondary relative z-0 flex-col justify-between space-y-8 rounded-b-xl px-8 py-6 shadow'>
