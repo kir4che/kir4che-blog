a5b357a2b7102e99573c598298979284662ee879
kir4che <mollydcxxiii@gmail.com>
2025-10-06 01:43:57 +0800

    ✨ add SmallText component, and integrate into MDX layouts

--- src/components/mdx/Accordion.tsx
+++ src/components/mdx/Accordion.tsx
@@ -4,7 +4,7 @@ import { Triangle } from 'lucide-react';
 import { cn } from '@/lib/style';
 
 interface AccordionProps {
-  variant?: 'default' | 'primary' | 'secondary';
+  variant?: 'default' | 'basic' | 'pink';
   title: string;
   children: React.ReactNode;
   className?: string;
@@ -14,34 +14,32 @@ const accordionStyles = {
   container: {
     base: 'group collapse rounded-none',
     variants: {
-      default: 'bg-black/0',
-      primary: 'border border-pink-300 bg-black/0 dark:border-pink-400/50',
-      secondary: 'text-text-secondary',
+      default: 'border border-pink-300 bg-black/0 dark:border-pink-400/50',
+      basic: 'bg-black/0',
+      pink: 'text-text-secondary',
     },
   },
   title: {
     base: 'collapse-title font-semibold text-base/4',
     variants: {
-      default: 'px-0 text-text-primary',
-      primary:
+      default:
         'peer-checked:border-b border-pink-300 px-3 text-pink-700 dark:border-pink-400/50 dark:text-pink-200',
-      secondary:
-        'rounded-md border border-pink-300 bg-pink-50 py-2.5 text-sm text-pink-700 peer-checked:rounded-b-none peer-checked:border-b-0 dark:border-pink-400/50 dark:bg-pink-900/10 dark:text-pink-100',
+      basic: 'px-0 text-text-primary',
+      pink: 'rounded-md border border-pink-300 bg-pink-50 py-2.5 text-sm text-pink-700 peer-checked:rounded-b-none peer-checked:border-b-0 dark:border-pink-400/50 dark:bg-pink-900/10 dark:text-pink-100',
     },
   },
   content: {
     base: 'collapse-content',
     variants: {
-      default: 'px-0 peer-checked:py-3',
-      primary: 'px-3 peer-checked:py-3',
-      secondary:
-        'rounded-b-md border-x border-b border-pink-300 bg-pink-50 py-3 text-pink-700 dark:border-pink-400/50 dark:bg-pink-900/10 dark:text-pink-100',
+      default: 'px-3 peer-checked:py-3',
+      basic: 'px-0 peer-checked:py-3',
+      pink: 'rounded-b-md border-x border-b border-pink-300 bg-pink-50 py-3 text-pink-700 dark:border-pink-400/50 dark:bg-pink-900/10 dark:text-pink-100',
     },
   },
 };
 
 const Accordion: React.FC<AccordionProps> = ({
-  variant = 'primary',
+  variant = 'default',
   title,
   children,
   className,
