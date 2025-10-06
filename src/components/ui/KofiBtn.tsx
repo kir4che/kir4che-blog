79d32542512c2d82c5c6ec6989163e0816a5139a
kir4che <mollydcxxiii@gmail.com>
2025-10-06 10:57:22 +0800

    🌐 add localization for Kofi button text

--- src/components/ui/KofiBtn.tsx
+++ src/components/ui/KofiBtn.tsx
@@ -1,25 +1,31 @@
 import Image from 'next/image';
+import { useTranslations } from 'next-intl';
 
+import { CONFIG } from '@/config';
 import ExternalLink from '@/components/ui/ExternalLink';
 
 import KofiIcon from '../../../public/svg/kofi.svg';
 
-const KofiBtn = () => (
-  <ExternalLink
-    href='https://ko-fi.com/yangshun'
-    title='Support me on Ko-fi'
-    className='text-text-secondary dark:text-text-secondary inline-flex w-fit items-center gap-x-2 rounded-md bg-pink-500 px-3 py-2 text-sm font-semibold hover:bg-pink-500/90 hover:no-underline'
-  >
-    <Image
-      src={KofiIcon}
-      alt='Ko-fi'
-      width={20}
-      height={20}
-      className='size-5'
-      aria-hidden='true'
-    />
-    Buy me a milktea
-  </ExternalLink>
-);
+const KofiBtn = () => {
+  const t = useTranslations('common');
+
+  return (
+    <ExternalLink
+      href={`https://ko-fi.com/${CONFIG.siteInfo.name}`}
+      title='Support me on Ko-fi'
+      className='text-text-secondary dark:text-text-secondary inline-flex w-fit items-center gap-x-2 rounded-md bg-pink-500 px-3 py-2 text-sm font-semibold hover:bg-pink-500/90 hover:no-underline'
+    >
+      <Image
+        src={KofiIcon}
+        alt='Ko-fi'
+        width={20}
+        height={20}
+        className='size-5'
+        aria-hidden='true'
+      />
+      {t('kofiButton')}
+    </ExternalLink>
+  );
+};
 
 export default KofiBtn;
