caf3aba579f0a0bb5f9d70e1516e154041a15a54
kir4che <mollydcxxiii@gmail.com>
2026-07-16 00:50:30 +0800

    💄 refactor headings, mdx-content, and typography in globals.css

--- src/components/mdx/Headings.tsx
+++ src/components/mdx/Headings.tsx
@@ -5,38 +5,9 @@ interface HeadingProps {
   children?: ReactNode;
 }
 
-export const H1 = ({ id, children }: HeadingProps) => (
-  <h1 id={id} className="heading my-5 -ml-4 md:-ml-6">
-    {children}
-  </h1>
-);
-
-export const H2 = ({ id, children }: HeadingProps) => (
-  <h2 id={id} className="heading my-5 -ml-4 md:-ml-6">
-    {children}
-  </h2>
-);
-
-export const H3 = ({ id, children }: HeadingProps) => (
-  <h3 id={id} className="heading my-5 -ml-4 md:-ml-6">
-    {children}
-  </h3>
-);
-
-export const H4 = ({ id, children }: HeadingProps) => (
-  <h4 id={id} className="heading my-5 -ml-4 md:-ml-6">
-    {children}
-  </h4>
-);
-
-export const H5 = ({ id, children }: HeadingProps) => (
-  <h5 id={id} className="heading my-5 -ml-4 md:-ml-6">
-    {children}
-  </h5>
-);
-
-export const H6 = ({ id, children }: HeadingProps) => (
-  <h6 id={id} className="heading my-5 -ml-4 md:-ml-6">
-    {children}
-  </h6>
-);
+export const H1 = ({ id, children }: HeadingProps) => <h1 id={id}>{children}</h1>;
+export const H2 = ({ id, children }: HeadingProps) => <h2 id={id}>{children}</h2>;
+export const H3 = ({ id, children }: HeadingProps) => <h3 id={id}>{children}</h3>;
+export const H4 = ({ id, children }: HeadingProps) => <h4 id={id}>{children}</h4>;
+export const H5 = ({ id, children }: HeadingProps) => <h5 id={id}>{children}</h5>;
+export const H6 = ({ id, children }: HeadingProps) => <h6 id={id}>{children}</h6>;
