b83c70ef56de97ec964f2a58af6e937b1da31799
kir4che <mollydcxxiii@gmail.com>
2026-07-16 00:50:31 +0800

    🐛 use youtube-nocookie embeds to restore video thumbnails

--- src/data/youtubes.ts
+++ src/data/youtubes.ts
@@ -1,17 +1,17 @@
 const youtubes = {
   1: {
     title: '七月份小聚帕斯塔焗麵所 & 最愛拍貼店 GOOD KIDS CLUB 🧸ྀི | Molly',
-    url: 'https://www.youtube.com/embed/t_qMUo__cWE?si=hf7EiK1BooHcEWgP',
+    url: 'https://www.youtube-nocookie.com/embed/t_qMUo__cWE?si=hf7EiK1BooHcEWgP',
     date: '2025.08.25',
   },
   2: {
     title: '生日月小聚 Waku Waku Pasta 🍝| Molly',
-    url: 'https://www.youtube.com/embed/Xs1CVvvXvVM?si=wLrdOw2rmYhFHPYL',
+    url: 'https://www.youtube-nocookie.com/embed/Xs1CVvvXvVM?si=wLrdOw2rmYhFHPYL',
     date: '2025.08.20',
   },
   3: {
     title: '五月份小聚 Waku Waku Burger (っᐛ)っ🍔 | Molly',
-    url: 'https://www.youtube.com/embed/M6r9dyYaXvI?si=HMIs4G5rXUWT8YOc',
+    url: 'https://www.youtube-nocookie.com/embed/M6r9dyYaXvI?si=HMIs4G5rXUWT8YOc',
     date: '2025.06.26',
   },
 };
